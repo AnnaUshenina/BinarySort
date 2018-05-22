1c0b1ddcb6f8aac927cee54d9cc134a14d9a9185
Ushenina Anna <ushenina.anna@mail.ru>
2018-05-23 00:11:31 +0500

    все плохо

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+//ВСЕ ОЧЕНЬ ПЛОХО
 namespace BinarySearch
 {
     class Program
